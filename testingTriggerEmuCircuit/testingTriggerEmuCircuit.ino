7e35b6b005e93472e92bc4c30284a1c83bae856b
jphran <41977074+jphran@users.noreply.github.com>
2019-10-25 12:59:37 -0600

    updated test file for motor lib

--- testingTriggerEmuCircuit/testingTriggerEmuCircuit.ino
+++ testingTriggerEmuCircuit/testingTriggerEmuCircuit.ino
@@ -9,22 +9,27 @@
 */
  
 
-const int DIR_PIN = 2;
+#include <Motor.h>
  
 void setup()
 {   
     Serial.begin(115200);
-    analogWriteResolution(12);
-    pinMode(A14, OUTPUT);
+    // analogWriteResolution(12);
+    // pinMode(A14, OUTPUT);
 }
 
+Motor motor;
 float i = 0;
 void loop()
 {
-    int val = (int) (4096/2*(.5/3.3)*sin(i)+(4096/2*(3/3.3)));
-    pinMode(3, OUTPUT);
-    analogWrite(A14, val);
-    delay(10);
-    Serial.println(val);
-    i += .01;
+    // int val = (int) (4096/2*(.5/3.3)*sin(i)+(4096/2*(3/3.3)));
+    // pinMode(3, OUTPUT);
+    // analogWrite(A14, val);
+    // delay(10);
+    // Serial.println(val);
+    // i += .01;
+    float effort = (.25*sin(i));
+    i += 0.0001;
+    Serial.println(effort);
+    // motor.Drive(effort);
 }
